b2448405c66666c16a84f120c302802241accb80
koctz <137113690+koctz@users.noreply.github.com>
2026-01-22 23:20:06 +0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,14 +1,19 @@
 import asyncio
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
+from aiogram.client.default import DefaultBotProperties
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from bot.config import BOT_TOKEN
 from bot.handlers.start import router as start_router
 from bot.handlers.youtube import router as youtube_router
 
+
 async def main():
-    bot = Bot(token=BOT_TOKEN, parse_mode=ParseMode.HTML)
+    bot = Bot(
+        token=BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
     dp = Dispatcher(storage=MemoryStorage())
 
     dp.include_router(start_router)
@@ -16,6 +21,6 @@ async def main():
 
     await dp.start_polling(bot)
 
+
 if __name__ == "__main__":
     asyncio.run(main())
-
