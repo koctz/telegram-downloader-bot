50971adbb2dd91b4ca7de9508237ba1d736be37f
koctz <137113690+koctz@users.noreply.github.com>
2026-01-22 22:23:26 +0500

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMINS = [x for x in os.getenv("ADMINS", "").split(",") if x]
+
+DOWNLOAD_DIR = os.getenv("DOWNLOAD_DIR", "downloads")
 
